66485a30cbf9f3b484e4c6647b4a36eaaf976ee8
Artyom.Evsyukov <artyom.evsyukov@gmail.com>
2022-12-09 10:34:15 +0300

    added button btnDarkMode and position

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const btnDarkMode = document.querySelector(".dark-mode-btn");
+
+btnDarkMode.onclick = function () {
+  //   console.log("Click");
+  btnDarkMode.classList.toggle("dark-mode-btn--active");
+};
