1d444fd4624c7cd17de05b91ca21a836e59cbd6d
Artyom.Evsyukov <artyom.evsyukov@gmail.com>
2022-12-09 13:20:37 +0300

    Dark mode localStorage

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
 const btnDarkMode = document.querySelector(".dark-mode-btn");
 
+//Проверка темной темы в localStorage
+if (localStorage.getItem("darkMode") === "dark") {
+  btnDarkMode.classList.add("dark-mode-btn--active");
+  document.body.classList.add("dark");
+}
+// else if (localStorage.getItem("darkMode") === "light") {
+//   btnDarkMode.classList.add("dark-mode-btn--active");
+//   document.body.add("light");
+// }
+
+//Проверка системной темы
+
+//Включение ночного режима по кнопки
 btnDarkMode.onclick = function () {
   //   console.log("Click");
   btnDarkMode.classList.toggle("dark-mode-btn--active");
+  const isDark = document.body.classList.toggle("dark");
+
+  if (isDark) {
+    localStorage.setItem("darkMode", "dark");
+  } else {
+    localStorage.setItem("darkMode", "light");
+  }
 };
